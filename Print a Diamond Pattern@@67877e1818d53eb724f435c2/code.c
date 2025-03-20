6c755eeb9261224a88faa3296b31d4dbca06c997
specksblue <namansingh.pb@gmail.com>
2025-03-20 18:10:01 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 void printDiamond(int n) {
-    for (int i = 1; i <= n; i += 2) {
-        for (int j = 0; j < (n - i) / 2; j++) printf(" ");
+    int maxWidth = 2 * n - 1; // Ensures 5 lines when n=3
+    for (int i = 1; i <= maxWidth; i += 2) {
+        for (int j = 0; j < (maxWidth - i) / 2; j++) printf(" ");
         for (int j = 0; j < i; j++) printf("*");
         printf("\n");
     }
-    for (int i = n - 2; i > 0; i -= 2) {
-        for (int j = 0; j < (n - i) / 2; j++) printf(" ");
+    for (int i = maxWidth - 2; i > 0; i -= 2) {
+        for (int j = 0; j < (maxWidth - i) / 2; j++) printf(" ");
         for (int j = 0; j < i; j++) printf("*");
         printf("\n");
     }
@@ -15,7 +16,7 @@ void printDiamond(int n) {
 
 int main() {
     int n;
-    scanf("%d", &n); // Input for number of rows (odd number recommended)
+    scanf("%d", &n); // Input for half-diamond height
     printDiamond(n);
     return 0;
 }
