5e5b931bd237c7e0880209a632bbe5079ef77c77
specksblue <namansingh.pb@gmail.com>
2025-03-20 18:09:07 +0530

    Solution to Print a Diamond Pattern

--- Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
+++ Print a Diamond Pattern@@67877e1818d53eb724f435c2/code.c
@@ -1,40 +1,21 @@
 #include <stdio.h>
 
-int main() {
-    int n;
-    scanf("%d", &n); // Input for diamond height (must be an odd number)
-
-    int spaces = n / 2;
-    int stars = 1;
-
-    // Upper half
-    for (int i = 1; i <= (n / 2) + 1; i++) {
-        for (int j = 1; j <= spaces; j++) {
-            printf(" ");
-        }
-        for (int j = 1; j <= stars; j++) {
-            printf("*");
-        }
+void printDiamond(int n) {
+    for (int i = 1; i <= n; i += 2) {
+        for (int j = 0; j < (n - i) / 2; j++) printf(" ");
+        for (int j = 0; j < i; j++) printf("*");
         printf("\n");
-        spaces--;
-        stars += 2;
     }
-
-    // Lower half
-    spaces = 1;
-    stars = n - 2;
-    for (int i = 1; i <= n / 2; i++) {
-        for (int j = 1; j <= spaces; j++) {
-            printf(" ");
-        }
-        for (int j = 1; j <= stars; j++) {
-            printf("*");
-        }
+    for (int i = n - 2; i > 0; i -= 2) {
+        for (int j = 0; j < (n - i) / 2; j++) printf(" ");
+        for (int j = 0; j < i; j++) printf("*");
         printf("\n");
-        spaces++;
-        stars -= 2;
     }
-
-    return 0;
 }
 
+int main() {
+    int n;
+    scanf("%d", &n); // Input for number of rows (odd number recommended)
+    printDiamond(n);
+    return 0;
+}
